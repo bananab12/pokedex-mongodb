f8115fec780c51884898415ac895dd41884cc3d9
Hugo Orlando Gonzalez <hugo-orlando.gonzalez@atos.net>
2023-11-13 17:00:20 -0300

    :zap: add pokemon resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
-    // index.html
+    // servir el index.html que esta en el folder public
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    PokemonModule,
   ],
   controllers: [],
   providers: [],
