666b5020237c7a3c739959d7af755d06f010683b
Hugo Orlando Gonzalez <hugo-orlando.gonzalez@atos.net>
2023-11-14 17:08:34 -0300

    :rocket: added AxiosAdapter & HttpAdapter

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import axios, { AxiosInstance } from 'axios';
 
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { AxiosAdapter } from 'src/common/adapter/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
-
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
 
   async executeSeed() {
