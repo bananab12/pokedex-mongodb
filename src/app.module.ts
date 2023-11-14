a65452c649f4c29fb3ef7d140363bc6efb25871f
Hugo Orlando Gonzalez <hugo-orlando.gonzalez@atos.net>
2023-11-14 09:56:39 -0300

    :zap: created a pokemon in bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
@@ -9,6 +11,8 @@ import { PokemonModule } from './pokemon/pokemon.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    // Referencia a la base de datos
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule,
   ],
   controllers: [],
--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,15 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+// Colección en MongoDB
+@Schema()
+export class Pokemon extends Document {
+  @Prop({ unique: true, indexed: true })
+  name: string;
+
+  @Prop({ unique: true, indexed: true })
+  no: number; // numero de pokemon
+}
+
+// exportar un Schema
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
