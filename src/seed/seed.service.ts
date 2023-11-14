9b10730f2f0ede07d68a2e895c50070517698d40
Hugo Orlando Gonzalez <hugo-orlando.gonzalez@atos.net>
2023-11-14 17:07:10 -0300

    :rocket: added AxiosAdapter & HttpAdapter

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -16,16 +16,23 @@ export class SeedService {
   ) {}
 
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.com/api/v2/pokemon?limit=650',
+    await this.pokemonModel.deleteMany({}); // delete * from pokemons;
+
+    const data = await this.http.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
-    data.results.forEach(async ({ name, url }) => {
+    const pokemonToInsert: { name: string; no: number }[] = [];
+
+    data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
-      const pokemon = await this.pokemonModel.create({ name, no });
+
+      pokemonToInsert.push({ name, no }); // [{ name: bulbasaur, no: 1 }]
     });
 
-    return 'Seed executed';
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
+    return 'Seed Executed';
   }
 }
