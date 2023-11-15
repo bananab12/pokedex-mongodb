d53a14d2b4262099d4c6faf4498bb31f116199d5
Hugo Orlando Gonzalez <hugo-orlando.gonzalez@atos.net>
2023-11-15 17:06:52 -0300

    :rocket: added configService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { join } from 'path'; // en Node
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
@@ -9,6 +10,7 @@ import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('Main (main.ts)');
+
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api/v2');
+  app.setGlobalPrefix('api/v1');
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -18,6 +22,9 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = parseInt(configService.get('PORT'));
+  await app.listen(port);
+  logger.log(`Server running on port ${port}`);
 }
 bootstrap();
